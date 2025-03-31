d88ad1eca15663513eda10a255477541afdf0155
marmaryia <marozavamaryia@gmail.com>
2025-03-31 15:07:20 +0100

    add linking to individual article pages

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getAllArticles } from "../api";
+import { Link } from "react-router-dom";
 
 function ArticlesList() {
   const [articles, setArticles] = useState([]);
@@ -22,15 +23,22 @@ function ArticlesList() {
       <div className="titles-container">
         {articles.map((article) => {
           return (
-            <div key={article.article_id} className="title-card">
-              <h2 className="title">{article.title}</h2>
-              <p>Date: {String(new Date(article.created_at)).split("+")[0]}</p>
-              <p>Topic: {article.topic}</p>
-              <p>By: {article.author}</p>
-              <p>
-                📑: {article.comment_count} | ❤️: {article.votes}
-              </p>
-            </div>
+            <Link
+              to={`/articles/${article.article_id}`}
+              key={article.article_id}
+            >
+              <div className="title-card">
+                <h2 className="title">{article.title}</h2>
+                <p>
+                  Date: {String(new Date(article.created_at)).split("+")[0]}
+                </p>
+                <p>Topic: {article.topic}</p>
+                <p>By: {article.author}</p>
+                <p>
+                  📑: {article.comment_count} | ❤️: {article.votes}
+                </p>
+              </div>
+            </Link>
           );
         })}
       </div>
