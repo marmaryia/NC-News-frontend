a7d5057019a464acb63bd4409d849703da7c8436
marmaryia <marozavamaryia@gmail.com>
2025-04-01 10:50:34 +0100

    add error handling for voting

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,25 +1,31 @@
+import { useState } from "react";
 import { patchLikesCount } from "../api";
 
 function Voting({ id, setLikesCount }) {
-  function handleVote(e) {
-    const vote = Number(e.target.value);
+  const [error, setError] = useState(false);
 
+  function handleVote(e) {
+    setError(false);
+    const vote = Number(e.target.value);
     setLikesCount((currentLikes) => currentLikes + vote);
     patchLikesCount(id, vote).catch((err) => {
       setLikesCount((currentLikesCount) => currentLikesCount - vote);
-      setError("An error occurred. Please try again!");
+      setError("An error occurred. Please try again later");
     });
   }
 
   return (
-    <div className="voting-container">
-      <button className="voting-button" onClick={handleVote} value={1}>
-        + 1 💜
-      </button>
-      <button className="voting-button" onClick={handleVote} value={-1}>
-        - 1 💜
-      </button>
-    </div>
+    <>
+      <div className="voting-container">
+        <button className="voting-button" onClick={handleVote} value={1}>
+          + 1 ❤️
+        </button>
+        <button className="voting-button" onClick={handleVote} value={-1}>
+          – 1 ❤️
+        </button>
+      </div>
+      {error ? <p className="right-align">{error}</p> : null}
+    </>
   );
 }
 
