78e72c7a6e4dda111de1ab0e0546414536418869
marmaryia <marozavamaryia@gmail.com>
2025-04-05 16:22:05 +0100

    allow only one up or down vote per article per page session, apply style change for a disabled button

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,26 +3,45 @@ import { patchLikesCount } from "../api";
 
 function Voting({ id, setLikesCount, children }) {
   const [error, setError] = useState(false);
+  const [remainingVotes, setRemainingVotes] = useState({ up: 1, down: 1 });
 
   function handleVote(e) {
     setError(false);
     const vote = Number(e.target.value);
+
+    setRemainingVotes((current) => {
+      return { up: current.up - vote, down: current.down + vote };
+    });
+
     setLikesCount((currentLikes) => currentLikes + vote);
     patchLikesCount(id, vote).catch((err) => {
       setLikesCount((currentLikesCount) => currentLikesCount - vote);
       setError("An error occurred. Please try again later");
+      setRemainingVotes((current) => {
+        return { up: current.up + vote, down: current.down - vote };
+      });
     });
   }
 
   return (
     <>
       <div className="voting-container">
-        <button className="voting-button" onClick={handleVote} value={1}>
-          ➕
+        <button
+          className="voting-button"
+          onClick={handleVote}
+          value={-1}
+          disabled={remainingVotes.down ? false : true}
+        >
+          ➖
         </button>
         {children}
-        <button className="voting-button" onClick={handleVote} value={-1}>
-          ➖
+        <button
+          className="voting-button"
+          onClick={handleVote}
+          value={+1}
+          disabled={remainingVotes.up ? false : true}
+        >
+          ➕
         </button>
       </div>
       {error ? <p className="right-align">{error}</p> : null}
